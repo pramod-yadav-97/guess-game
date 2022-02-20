04b0913afcf94dd8c77f3bd57cf3c037ff3624b9
Pramod <Pramod@mail.com>
2022-02-20 16:51:07 +0530

    Calculator script

---
+++ cal.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+num1=$1
+num2=$2
+op=$3
+
+if [[ $# != 3 ]]
+then 
+	echo "Please pass 3 arguments!  Ex=>  $0 5 2 (add/mul/sub/div)"
+fi
+
+add() {
+
+sum=`expr $num1 + $num2`
+echo $sum
+
+}
+
+
+
+sub() 
+{
+if [[ $num1 > $num2 ]]
+then
+	sub=`expr $num1 - $num2`
+else
+	sub=`expr $num2 - $num1`
+fi
+echo $sub
+
+}
+
+
+mul() {
+
+	mul=$(($num1*$num2))
+echo $mul
+
+}
+
+div() {
+if [[ $num2 == 0 ]]
+then
+	echo "Anything Divided by zero is Zero !!"
+else
+
+div=`expr $num1 / $num2`
+echo $div
+fi
+
+}
+
+
+if [ $op == "add" ]
+then
+	echo "Addition is " 
+	add
+elif [ $op == "sub" ]
+then
+	echo "Subtraction is"
+	sub
+elif [ $op == "mul" ]
+then
+	echo "Multiplication is"
+	mul
+elif [ $op == "div" ]
+then
+	echo "Division is "
+	div
+else
+	echo "Please check the input provided !!"
+
+fi
+
+
